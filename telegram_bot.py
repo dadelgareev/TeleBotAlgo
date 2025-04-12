97038578768d8a5752d6eca1d60dbe47ca4fd14d
dadelgareev <139441933+dadelgareev@users.noreply.github.com>
2025-04-12 14:25:15 +0500

    Добавлен основной скрипт бота, конфиг файл с ключами

---
+++ telegram_bot.py
@@ -0,0 +1,97 @@
+import asyncio
+from random import randint
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+import requests
+import json
+
+#Получаем ключ weather api и ключ telegram бота
+with open("config.json", "r") as f:
+    config = json.load(f)
+    BOT_TOKEN = config["BOT_KEY"]
+    WEATHER_API_KEY = config["WEATHER_API_KEY"]
+
+# Функция стартового сообщения
+async def start(update: Update, context):
+    await update.message.reply_text('Привет! Я тестовый бот. Напиши что-нибудь!')
+
+# Эхо-команда
+async def echo(update: Update, context):
+    word_user = update.message.text
+    word_reserve = word_user[::-1]
+    await update.message.reply_text(f'Ты написал: {word_reserve}')
+
+# Угадай число
+async def guess_number(update: Update, context):
+    if not context.args:
+        await update.message.reply_text("Укажите число для угадывания")
+        return
+
+    if not context.user_data.get("number"):
+        context.user_data["number"] = str(randint(1, 10))
+
+    if int(context.args[0]) > int(context.user_data["number"]):
+        await update.message.reply_text("Число меньше")
+    elif int(context.args[0]) < int(context.user_data["number"]):
+        await update.message.reply_text("Число больше")
+    else:
+        await update.message.reply_text("Поздравляю, вы угадали! Число сброшено.")
+        context.user_data["number"] = str(randint(1, 10))
+
+# Таймер
+async def settimer(update: Update, context):
+    if not context.args:
+        await update.message.reply_text("Вы должны ввести команду в формате /settimer <секунды>")
+        return
+
+    seconds = int(context.args[0])
+    await update.message.reply_text(f"Таймер на {seconds} секунд установлен!")
+    await asyncio.sleep(seconds)
+    await update.message.reply_text("Таймер сработал!")
+
+# Команда погоды
+async def get_weather(update: Update, context):
+    if not context.args:
+        await update.message.reply_text("Вы должны ввести команду в формате /getWeather <название города>")
+        return
+
+    city = context.args[0]
+    url = 'http://api.weatherapi.com/v1/current.json'
+    aqi = 'yes'
+    params = {
+        'key': WEATHER_API_KEY,
+        'q': city,
+        'aqi': aqi
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+
+        data = response.json()
+        message = f"""
+        Город: {data['location']['name']},
+        Регион: {data['location']['region']},
+        Время: {data['location']['localtime']},
+        Температура: {data['current']['temp_c']}°C,
+        Облачность: {data['current']['cloud']},
+        Влажность: {data['current']['humidity']}%,
+        Ветер: {data['current']['wind_kph']} км/ч
+        """
+        await update.message.reply_text(message)
+    except Exception as e:
+        await update.message.reply_text(f"Ошибка при получении погоды: {e}")
+
+# Главная функция для запуска бота
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(CommandHandler("guess", guess_number))
+    application.add_handler(CommandHandler("settimer", settimer))
+    application.add_handler(CommandHandler("getWeather", get_weather))
+    print("Бот запущен")
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
