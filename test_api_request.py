3cd85650a4a08b685a3cdac1e00c6ab2dae19eb9
dadelgareev <139441933+dadelgareev@users.noreply.github.com>
2025-05-11 13:16:34 +0500

    AI

--- test_api_request.py
+++ test_api_request.py
@@ -19,15 +19,21 @@ payload = [
         "width": 512,
         "height": 512,
         "model": "runware:100@1",
-        "numberResults": 1
+        "numberResults": 1,
+        "outputFormat": "PNG"
     }
 ]
 
 response = requests.post(URL, headers=headers, json=payload)
+
+image_url = None
 
 if "data" in response.json():
     data = response.json().get('data')[0]
     image_url = data.get('imageURL')
     print(image_url)
 
+response_image = requests.get(image_url)
+with open('test.png', 'wb') as f:
+    f.write(response_image.content)
 #print(response.json())
